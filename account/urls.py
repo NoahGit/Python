a6692b44e1658acc17eef4ab73d89d03285d0e04
noah <gaoyong@xatu.edu.cn>
2018-11-17 10:06:25 +0800

    Signed-off-by: 201811171006

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # url(r'^login/$', views.user_login, name="user_login"),    #-----自己写的login方法
     url(r'^login/$', auth_views.login, name="user_login"),      #使用django内置的登录方法，不是login.html登录页面
-    url(r'^new-login/$', auth_views.login, {"template_name": "account/login.html"}),
-    url(r'^logout/$', auth_views.logout, name='user_logout'),
+    url(r'^new-login/$', auth_views.login, {"template_name": "account/login.html"}, name="user_login_new"),
+    url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"}, name='user_logout'),
 ]
