5194bedff6bfa40677daf02abf39aaf23b174442
noah <gaoyong@xatu.edu.cn>
2018-11-24 21:30:06 +0800

    20181124-92page

--- account/urls.py
+++ account/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     url(r'^register/$', views.register, name="user_register"),
     url(r'^password-change/$', auth_views.password_change, {"post_change_redirect":"/account/password-change-done"}, name='password_change'),
     url(r'^password-change-done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^password-reset/$', auth_views.password_reset, {"post_reset_redirect":"/account/password-reset-done"}, name="password_reset"),
+    url(r'^password-reset-done/$', auth_views.password_reset_done, name="password_reset_done"),
+    url(r'^password-reset-confirm/$', auth_views.password_reset_confirm, name="password_reset_confirm"),
+    url(r'^password-reset-complete/$', auth_views.password_reset_complete, name="password_reset_complete"),
 ]
