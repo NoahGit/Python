702acdcd07ec03af001ba2082009de6649f48ce6
noah <gaoyong@xatu.edu.cn>
2018-12-01 22:08:40 +0800

    20181201-92page

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,28 +142,3 @@ EMAIL_HOST_PASSWORD = 'zpvdczodqubrbijf'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = '42967148@qq.com'
-
-
-
-
-# from email.mime.text import MIMEText
-# msg = MIMEText('account/test.py 写发邮件', 'plain', 'utf-8')
-# # 输入Email地址和口令:
-# # from_addr = input('From: ')
-# from_addr = '42967148@qq.com'
-# # password = input('Password: ')
-# password = 'zpvdczodqubrbijf'
-# # 输入SMTP服务器地址:
-# # smtp_server = input('SMTP server: ')
-# smtp_server = 'smtp.qq.com'
-# # 输入收件人地址:
-# # to_addr = input('To: ')
-# to_addr = 'c5t4v@163.com'
-#
-# import smtplib
-# server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
-# server.set_debuglevel(1)
-# server.starttls()
-# server.login(from_addr, password)
-# server.sendmail(from_addr, [to_addr], msg.as_string())
-# server.quit()
